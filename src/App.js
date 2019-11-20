4ddd0855441abf0e0479d84b7ccae4392e74e270
amgad behman <amgadbehman12345@gmail.com>
2019-11-19 22:52:44 -0800

    color-changer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,45 @@
-import React, { Component } from 'react'
-import HelloWorld from './components/HelloWorld'
+import React, { component, useState } from 'react'
 
-class App extends Component {
-  render() {
-    return <HelloWorld />
+import Slider from './components/Slider'
+import { changeExt } from 'upath'
+
+const App = () => {
+  const getRandom = () => {
+    return Math.floor(Math.random() * 100)
   }
+
+  let [hsl, setHSL] = useState(getRandom())
+  let [saturation, setSATURATION] = useState(getRandom())
+  let [light, setLIGHT] = useState(getRandom())
+
+  const changeSetHSL = event => {
+    setHSL(event.target.value)
+  }
+
+  const changeSaturation = event => {
+    setSATURATION(event.target.value)
+  }
+
+  const changeLight = event => {
+    setLIGHT(event.target.value)
+  }
+
+  const setRandom = () => {
+    setHSL(getRandom())
+    setSATURATION(getRandom())
+    setLIGHT(getRandom())
+  }
+
+  return (
+    <div className="whole"  style={{ backgroundColor: `hsl(${hsl},${saturation}%,${light}%)` }}>
+      <h1>Colors</h1>
+      <Slider value={hsl} change={changeSetHSL} />
+      <Slider value={saturation} change={changeSaturation} />
+      <Slider value={light} change={changeLight} />
+      <br />
+      <button onClick={setRandom}>RANDOM</button>
+    </div>
+  )
 }
 
 export default App
--- src/components/HelloWorld.js
+++ src/components/HelloWorld.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 
+
 class HelloWorld extends Component {
   render() {
     return <h1>Hello, World!</h1>
