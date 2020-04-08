cba246186c6fbd3a949d6b4dbd1aa48d0d08169d
amgad behman <amgadbehman12345@gmail.com>
2020-04-07 18:06:17 -0700

    done

--- src/App.js
+++ src/App.js
@@ -1,45 +1,84 @@
 import React, { component, useState } from 'react'
-
 import Slider from './components/Slider'
-import { changeExt } from 'upath'
 
 const App = () => {
-  const getRandom = () => {
-    return Math.floor(Math.random() * 100)
-  }
+ 
+  const [hsl, setHSL]= useState(Math.floor(Math.random() * 360))
+  const [saturation, setSATURATION] = useState(Math.floor(Math.random() * 101))
+  const [light, setLIGHT] = useState(Math.floor(Math.random() * 101))
+  const [alpha, setAlphaLevel] = useState(Math.floor(Math.random() * 101))
 
-  let [hsl, setHSL] = useState(getRandom())
-  let [saturation, setSATURATION] = useState(getRandom())
-  let [light, setLIGHT] = useState(getRandom())
+  var randomnumber = Math.floor(Math.random() * (1 - 1000 + 1)) + 1000;
+console.log(randomnumber)
 
-  const changeSetHSL = event => {
+  const changeSetHSL = (event) => {
     setHSL(event.target.value)
   }
 
-  const changeSaturation = event => {
+  const changeSaturation = (event) => {
     setSATURATION(event.target.value)
   }
 
-  const changeLight = event => {
+  const changeLight = (event) => {
     setLIGHT(event.target.value)
   }
 
-  const setRandom = () => {
-    setHSL(getRandom())
-    setSATURATION(getRandom())
-    setLIGHT(getRandom())
+
+  const changeAlpha = (event) => {
+    setAlphaLevel(event.target.value)
   }
 
+
+
+  const randomColor = () => {
+    setHSL(() => Math.floor(Math.random() * 360 + 1))
+    setSATURATION(() => Math.floor(Math.random() * 101))
+    setLIGHT(() => Math.floor(Math.random() * 101))
+    setAlphaLevel(() => Math.floor(Math.random() * 101))
+    
+  }
+
+
+  const buttonRandomColor = () => {
+    setHSL(() => Math.floor(Math.random() * 360 + 1))
+    setSATURATION(() => Math.floor(Math.random() * 101))
+    setLIGHT(() => Math.floor(Math.random() * 101))
+    setAlphaLevel(() => Math.floor(Math.random() * 101))
+    
+  }
   return (
-    <div className="whole"  style={{ backgroundColor: `hsl(${hsl},${saturation}%,${light}%)` }}>
-      <h1>Colors</h1>
+    <>
+    <body>
+    <header  >
+      Because life's too short to live in black and white
+    </header>
+    <div className = "img-Container">
+    <div
+      className='whole'
+      style={{ backgroundColor: `hsl(${hsl},${saturation}%,${light}%,${alpha}%)`}}
+    >
       <Slider value={hsl} change={changeSetHSL} />
       <Slider value={saturation} change={changeSaturation} />
       <Slider value={light} change={changeLight} />
+      <Slider value={alpha} change={changeAlpha} />
       <br />
-      <button onClick={setRandom}>RANDOM</button>
+      <div className = "button-Container">
+      <button
+       className="button"
+       style={{ backgroundColor: `hsl(${hsl*randomnumber},${saturation*randomnumber}%,${light}%,${alpha*randomnumber}%)`}}
+       onClick={randomColor}>
+        RANDOM
+     </button>
+     </div>
+     </div>
+     <footer>
+       Colors are vibrating light energies, each “color ray” produces a sound that affects matter.
+    </footer>
     </div>
+    </body>
+    </>
   )
 }
+
 
 export default App
